a9ead2ac8b4844635e767f5487088da96985a0d6
Cristian Huichaqueo <cristianhuichaqueo@gmail.com>
2018-12-29 16:20:52 -0300

    new objects

--- generators/spring-controller/index.js
+++ generators/spring-controller/index.js
@@ -1,77 +1,144 @@
 /* eslint-disable consistent-return */
+const _ = require('lodash');
 const chalk = require('chalk');
-const SpringControllerGenerator = require('generator-jhipster/generators/spring-controller');
+const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const constants = require('generator-jhipster/generators/generator-constants');
+const statistics = require('generator-jhipster/generators/statistics');
+const prompts = require('./prompts');
 
-module.exports = class extends SpringControllerGenerator {
+const SERVER_MAIN_SRC_DIR = `${constants.MAIN_DIR}dotnet/`;
+const SERVER_TEST_SRC_DIR = `${constants.TEST_DIR}dotnet/`;
+
+module.exports = class extends BaseGenerator {
     constructor(args, opts) {
-        super(args, Object.assign({ fromBlueprint: true }, opts)); // fromBlueprint variable is important
+        super(args, Object.assign({ fromBlueprint: true }, opts));
+        this.argument('name', { type: String, required: true });
+        this.name = this.options.name;
 
-        const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
+        // This adds support for a `--from-cli` flag
+        this.option('from-cli', {
+            desc: 'Indicates the command is run from JHipster CLI',
+            type: Boolean,
+            defaults: false
+        });
+        this.option('default', {
+            type: Boolean,
+            default: false,
+            description: 'default option'
+        });
+        this.defaultOption = this.options.default;
+    }
 
-        if (!jhContext) {
-            this.error(
-                `This is a JHipster blueprint and should be used only like ${chalk.yellow(
-                    'jhipster --blueprint generator-jhipster-dotnet'
-                )}`
-            );
-        }
+    // Public API method used by the getter and also by Blueprints
+    _initializing() {
+        return {
+            validateFromCli() {
+                this.checkInvocationFromCLI();
+            },
 
-        this.configOptions = jhContext.configOptions || {};
+            initializing() {
+                this.log(`The spring-controller ${this.name} is being created.`);
+                const configuration = this.getAllJhipsterConfig(this, true);
+                const blueprintConfiguration = this.getJhipsterAppConfig('generator-jhipster-dotnet');
+                this.baseName = blueprintConfiguration.baseName || configuration.get('baseName');
+                this.packageName = blueprintConfiguration.packageName || configuration.get('packageName');
+                this.packageFolder = blueprintConfiguration.packageFolder || configuration.get('packageFolder');
+                this.databaseType = blueprintConfiguration.databaseType || configuration.get('databaseType');
+                this.reactiveController = false;
+                this.applicationType = blueprintConfiguration.applicationType || configuration.get('applicationType');
+                this.reactive = blueprintConfiguration.reactive || configuration.get('reactive');
+                this.reactiveController = this.reactive;
+                this.controllerActions = [];
+            }
+        };
     }
 
     get initializing() {
-        /**
-         * Any method beginning with _ can be reused from the superclass `SpringControllerGenerator`
-         *
-         * There are multiple ways to customize a phase from JHipster.
-         *
-         * 1. Let JHipster handle a phase, blueprint doesnt override anything.
-         * ```
-         *      return super._initializing();
-         * ```
-         *
-         * 2. Override the entire phase, this is when the blueprint takes control of a phase
-         * ```
-         *      return {
-         *          myCustomInitPhaseStep() {
-         *              // Do all your stuff here
-         *          },
-         *          myAnotherCustomInitPhaseStep(){
-         *              // Do all your stuff here
-         *          }
-         *      };
-         * ```
-         *
-         * 3. Partially override a phase, this is when the blueprint gets the phase from JHipster and customizes it.
-         * ```
-         *      const phaseFromJHipster = super._initializing();
-         *      const myCustomPhaseSteps = {
-         *          displayLogo() {
-         *              // override the displayLogo method from the _initializing phase of JHipster
-         *          },
-         *          myCustomInitPhaseStep() {
-         *              // Do all your stuff here
-         *          },
-         *      }
-         *      return Object.assign(phaseFromJHipster, myCustomPhaseSteps);
-         * ```
-         */
-        // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._initializing();
+        return this._initializing();
+    }
+
+    // Public API method used by the getter and also by Blueprints
+    _prompting() {
+        return {
+            askForControllerActions: prompts.askForControllerActions
+        };
     }
 
     get prompting() {
         // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._prompting();
+        return this._prompting();
+    }
+
+    // Public API method used by the getter and also by Blueprints
+    _default() {
+        return {
+            insight() {
+                statistics.sendSubGenEvent('generator', 'spring-controller-dotnet');
+            }
+        };
     }
 
     get default() {
         // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._default();
+        return this._default();
+    }
+
+    // Public API method used by the getter and also by Blueprints
+    _writing() {
+        return {
+            writing() {
+                this.controllerClass = _.upperFirst(this.name) + (this.name.endsWith('Resource') ? '' : 'Resource');
+                this.controllerInstance = _.lowerFirst(this.controllerClass);
+                this.apiPrefix = _.kebabCase(this.name);
+
+                if (this.controllerActions.length === 0) {
+                    this.log(chalk.green('No controller actions found, adding a default action'));
+                    this.controllerActions.push({
+                        actionName: 'defaultAction',
+                        actionMethod: 'Get'
+                    });
+                }
+
+                // helper for Java imports
+                this.usedMethods = _.uniq(this.controllerActions.map(action => action.actionMethod));
+                this.usedMethods = this.usedMethods.sort();
+
+                this.mappingImports = this.usedMethods.map(method => `org.springframework.web.bind.annotation.${method}Mapping`);
+                this.mockRequestImports = this.usedMethods.map(
+                    method => `static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.${method.toLowerCase()}`
+                );
+
+                this.mockRequestImports =
+                    this.mockRequestImports.length > 3
+                        ? ['static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*']
+                        : this.mockRequestImports;
+
+                this.mainClass = this.getMainClassName();
+
+                this.controllerActions.forEach(action => {
+                    action.actionPath = _.kebabCase(action.actionName);
+                    action.actionNameUF = _.upperFirst(action.actionName);
+                    this.log(
+                        chalk.green(
+                            `adding ${action.actionMethod} action '${action.actionName}' for /api/${this.apiPrefix}/${action.actionPath}`
+                        )
+                    );
+                });
+
+                this.template(
+                    `${SERVER_TEST_SRC_DIR}package/web/rest/ResourceIntTest.cs.ejs`,
+                    `${SERVER_TEST_SRC_DIR}${this.packageFolder}/web/rest/${this.controllerClass}IntTest.cs`
+                );
+                this.template(
+                    `${SERVER_MAIN_SRC_DIR}Controllers/Resource.cs.ejs`,
+                    `${SERVER_MAIN_SRC_DIR}${this.packageFolder}/web/rest/${this.controllerClass}.cs`
+                );
+            }
+        };
     }
 
     get writing() {
         // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._writing();
+        return this._writing();
     }
 };
--- generators/spring-service/index.js
+++ generators/spring-service/index.js
@@ -1,77 +1,118 @@
 /* eslint-disable consistent-return */
-const chalk = require('chalk');
-const SpringServiceGenerator = require('generator-jhipster/generators/spring-service');
+const _ = require('lodash');
+const BaseGenerator = require('generator-jhipster/generators/generator-base');
+const constants = require('generator-jhipster/generators/generator-constants');
+const statistics = require('generator-jhipster/generators/statistics');
 
-module.exports = class extends SpringServiceGenerator {
+const SERVER_MAIN_SRC_DIR = `${constants.MAIN_DIR}dotnet/`;
+
+module.exports = class extends BaseGenerator {
     constructor(args, opts) {
-        super(args, Object.assign({ fromBlueprint: true }, opts)); // fromBlueprint variable is important
+        super(args, Object.assign({ fromBlueprint: true }, opts));
+        this.argument('name', { type: String, required: true });
+        this.name = this.options.name;
 
-        const jhContext = (this.jhipsterContext = this.options.jhipsterContext);
+        // This adds support for a `--from-cli` flag
+        this.option('from-cli', {
+            desc: 'Indicates the command is run from JHipster CLI',
+            type: Boolean,
+            defaults: false
+        });
+        this.option('default', {
+            type: Boolean,
+            default: false,
+            description: 'default option'
+        });
+        this.defaultOption = this.options.default;
+    }
 
-        if (!jhContext) {
-            this.error(
-                `This is a JHipster blueprint and should be used only like ${chalk.yellow(
-                    'jhipster --blueprint generator-jhipster-dotnet'
-                )}`
-            );
-        }
+    _initializing() {
+        return {
+            validateFromCli() {
+                this.checkInvocationFromCLI();
+            },
 
-        this.configOptions = jhContext.configOptions || {};
+            initializing() {
+                this.log(`The service ${this.name} is being created.`);
+                const configuration = this.getAllJhipsterConfig(this, true);
+                const blueprintConfiguration = this.getJhipsterAppConfig('generator-jhipster-dotnet');
+                this.baseName = blueprintConfiguration.baseName || configuration.get('baseName');
+                this.packageName = blueprintConfiguration.packageName || configuration.get('packageName');
+                this.packageFolder = blueprintConfiguration.packageFolder || configuration.get('packageFolder');
+                this.databaseType = blueprintConfiguration.databaseType || configuration.get('databaseType');
+            }
+        };
     }
 
     get initializing() {
-        /**
-         * Any method beginning with _ can be reused from the superclass `SpringServiceGenerator`
-         *
-         * There are multiple ways to customize a phase from JHipster.
-         *
-         * 1. Let JHipster handle a phase, blueprint doesnt override anything.
-         * ```
-         *      return super._initializing();
-         * ```
-         *
-         * 2. Override the entire phase, this is when the blueprint takes control of a phase
-         * ```
-         *      return {
-         *          myCustomInitPhaseStep() {
-         *              // Do all your stuff here
-         *          },
-         *          myAnotherCustomInitPhaseStep(){
-         *              // Do all your stuff here
-         *          }
-         *      };
-         * ```
-         *
-         * 3. Partially override a phase, this is when the blueprint gets the phase from JHipster and customizes it.
-         * ```
-         *      const phaseFromJHipster = super._initializing();
-         *      const myCustomPhaseSteps = {
-         *          displayLogo() {
-         *              // override the displayLogo method from the _initializing phase of JHipster
-         *          },
-         *          myCustomInitPhaseStep() {
-         *              // Do all your stuff here
-         *          },
-         *      }
-         *      return Object.assign(phaseFromJHipster, myCustomPhaseSteps);
-         * ```
-         */
-        // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._initializing();
+        return this._initializing();
+    }
+
+    _prompting() {
+        return {
+            prompting() {
+                const prompts = [
+                    {
+                        type: 'confirm',
+                        name: 'useInterface',
+                        message: '(1/1) Do you want to use an interface for your service?',
+                        default: false
+                    }
+                ];
+                if (!this.defaultOption) {
+                    const done = this.async();
+                    this.prompt(prompts).then(props => {
+                        this.useInterface = props.useInterface;
+                        done();
+                    });
+                } else {
+                    this.useInterface = true;
+                }
+            }
+        };
     }
 
     get prompting() {
-        // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._prompting();
+        return this._prompting();
+    }
+
+    // Public API method used by the getter and also by Blueprints
+    _default() {
+        return {
+            insight() {
+                statistics.sendSubGenEvent('generator', 'service-dotnet', { interface: this.useInterface });
+            }
+        };
     }
 
     get default() {
-        // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._default();
+        return this._default();
+    }
+
+    // Public API method used by the getter and also by Blueprints
+    _writing() {
+        return {
+            write() {
+                this.serviceClass = _.upperFirst(this.name) + (this.name.endsWith('Service') ? '' : 'Service');
+                this.serviceInstance = _.lowerCase(this.serviceClass);
+
+                this.template(
+                    `${SERVER_MAIN_SRC_DIR}Services/Service.cs.ejs`,
+                    `${SERVER_MAIN_SRC_DIR + this.packageFolder}/Services/${this.serviceClass}.cs`
+                );
+
+                if (this.useInterface) {
+                    this.template(
+                        `${SERVER_MAIN_SRC_DIR}Services/Impl/ServiceImpl.cs.ejs`,
+                        `${SERVER_MAIN_SRC_DIR + this.packageFolder}/Services/Impl/${this.serviceClass}Impl.cs`
+                    );
+                }
+            }
+        };
     }
 
     get writing() {
-        // Here we are not overriding this phase and hence its being handled by JHipster
-        return super._writing();
+        return this._writing();
     }
+
 };
